1bdd9b429f24a59515a1a816ad1856c1e8d65858
Ram SE <ramsashie@gmail.com>
2026-08-24 21:47:09 -0400

    update swap.c with double and char swap.

--- module6/swap.c
+++ module6/swap.c
@@ -10,6 +10,17 @@ void broken_swap(int a, int b) {
     a = b;
     b = temp;
 }
+void swap_doubles(double *a, double *b) {
+    double temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void swap_chars(char *a, char *b) {
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 int main(void) {
     int x = 10;
@@ -23,5 +34,24 @@ int main(void) {
 
     broken_swap(x, y);
     printf("After broken_swap (values unchanged): x = %d, y = %d\n", x, y);
+
+    double a = 1.10;
+    double b = 2.11;
+
+    printf("Before swap_doubles: a = %.2f, b = %.2f\n", a, b);
+
+    swap_doubles(&a, &b);
+
+    printf("After swap_doubles:  a = %.2f, b = %.2f\n", a, b);
+
+    char char1 = 'A';
+    char char2= 'Z';
+
+    printf("Before swap_chars: c1 = %c, c2 = %c\n", char1, char2);
+
+    swap_chars(&char1, &char2);
+
+    printf("After swap_chars:  c1 = %c, c2 = %c\n", char1, char2);
+
     return 0;
 }
